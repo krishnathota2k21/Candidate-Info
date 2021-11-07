04062e85336458e1312b81fca5c918093145ab70
Krishna Thota <krishna.thota2k21@gmail.com>
2021-11-07 20:00:32 +0530

    parent reference added for children in mapper file.

--- src/main/java/com/ivispl/candidate/mapper/CandidateInfoMapper.java
+++ src/main/java/com/ivispl/candidate/mapper/CandidateInfoMapper.java
@@ -2,12 +2,21 @@ package com.ivispl.candidate.mapper;
 
 import com.ivispl.candidate.dto.CandidateInfoDto;
 import com.ivispl.candidate.entity.candidate.CandidateInfo;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(uses = {EducationMapper.class, EmploymentMapper.class, EmploymentOfferMapper.class, PassportDetailsMapper.class},
 componentModel = "spring")
 public interface CandidateInfoMapper {
-//    @Mapping(source = "passportDetails", target = "candidateInfo.passportDetails")
+
+    @AfterMapping
+    default void afterCandidateInfoMapping(@MappingTarget CandidateInfo candidateInfo){
+        candidateInfo.getEducationDetails().forEach(education -> education.setCandidateInfo(candidateInfo));
+        candidateInfo.getEmploymentHistory().forEach(employment -> employment.setCandidateInfo(candidateInfo));
+        candidateInfo.getEmploymentOffers().forEach(employmentOffer -> employmentOffer.setCandidateInfo(candidateInfo));
+        candidateInfo.getPassportDetails().setCandidateInfo(candidateInfo);
+    }
     CandidateInfo toEntity(CandidateInfoDto dto);
 
     CandidateInfoDto toDto(CandidateInfo entity);
